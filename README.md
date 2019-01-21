d697690f57e3b030a32ab5defb5496f2dbf8c85b
Jamie Lendrum <jalendrum@gmail.com>
2019-01-21 20:13:49 +0000

    updated script and readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # linux-video-scripts
+
+Code to generate scripts for chopping up and pasting together MP4 files on linux. It uses VLC Player to cut and MP4Box to join, as these programs work best for me.
+
+The workflow is:
+
+1. Place the two generate_scripts files in the same directory as the video;
+2. Create a text file called 'times.txt' where the first line is the filename of the video (with extension) and subsequent lines describe segments you want to keep (e.g. 1:01-3:04) 
+3. In the bash terminal: 'bash generate_scripts.sh'. This creates 'cut_script.sh' and 'merge_script.sh'; 
+4. In the bash terminal: 'bash cut_script.sh'. This generates numbered MP4 files.
+5. In the bash terminal: 'bash merge_script.sh'. This joins them all together to create 'merged.mp4'.
--- generate_scripts.R
+++ generate_scripts.R
@@ -1,29 +1,39 @@
-library(magrittr)
-library(dplyr, warn.conflicts = FALSE)
-library(tidyr, warn.conflicts = FALSE)
-library(stringr)
+suppressPackageStartupMessages(library(tidyverse))
 library(hms)
 
-#CUT IT UP
-times <- read.delim("times.txt", sep = "-", header = FALSE, col.names = c("start", "end"), skip = 1, stringsAsFactors = FALSE)
-
 video <- readLines("times.txt", n = 1)
+video <- if_else(str_detect(video, " "), str_c("'", video, "'"), video)
 
-full <- times %>%  mutate(start = ifelse(str_count(start, ":") == 1, paste0("0:", start), start)) %>% 
-                   mutate(end = ifelse(str_count(end, ":") == 1, paste0("0:", end), end)) %>%
-                   separate(start, c("start_hr", "start_min", "start_sec")) %>% 
-                   separate(end, c("end_hr", "end_min", "end_sec")) %>%
-                   mutate_all(as.numeric) %>% 
-                   mutate(start = 60*60*start_hr + 60*start_min + start_sec,
-                          end = 60*60*end_hr + 60*end_min + end_sec) %>%
-                   mutate(script1 = paste("vlc", 
-                                          video, 
-                                          "--start-time", 
-                                          start, 
-                                          "--stop-time", 
-                                          end),
-                          script2 = paste0(":sout=#file{dst=", row_number(), ".mp4}"),
-                          script = paste(script1, script2))
+#CUT IT UP
+full <-
+  read.delim(
+    "times.txt",
+    sep = "-",
+    header = FALSE,
+    col.names = c("start", "end"),
+    skip = 1,
+    stringsAsFactors = FALSE
+  ) %>%
+  mutate(start = if_else(str_count(start, ":") == 1, str_c("0:", start), start)) %>%
+  mutate(end = if_else(str_count(end, ":") == 1, str_c("0:", end), end)) %>%
+  separate(start, c("start_hr", "start_min", "start_sec")) %>%
+  separate(end, c("end_hr", "end_min", "end_sec")) %>%
+  mutate_all(as.numeric) %>%
+  mutate(
+    start = 60 * 60 * start_hr + 60 * start_min + start_sec,
+    end = 60 * 60 * end_hr + 60 * end_min + end_sec
+  ) %>%
+  mutate(
+    script1 = str_c("vlc",
+                    video,
+                    "--start-time",
+                    start,
+                    "--stop-time",
+                    end,
+                    sep = " "),
+    script2 = str_c(":sout=#file{dst=", row_number(), ".mp4}"),
+    script = str_c(script1, script2, sep = " ")
+  )
 
 #calculate total length of cut video 
 tot_hms <- full %>% 
@@ -31,19 +41,23 @@ tot_hms <- full %>%
             pull(tot) %>%
             hms()
 
-paste("# Total duration of video:", tot_hms) %>% 
+
+# GENERATE CUT SCRIPT -----------------------------------------------------
+
+str_c("# Total duration of video:", tot_hms, sep = " ") %>% 
   write.table("cut_script.sh", quote = FALSE, row.names = FALSE, col.names = FALSE)
 
 full %>% select(script) %>% 
            write.table("cut_script.sh", append = TRUE, quote = FALSE, row.names = FALSE, col.names = FALSE)
 
-#PASTE IT TOGETHER
+# GENERATE PASTE SCRIPT ---------------------------------------------------
+
 merge_script <- "MP4Box"
 
-for(i in 1:(nrow(times)-1)){
-  merge_script <- paste0(merge_script, " -cat ", i, ".mp4")
+for(i in 1:nrow(full)) {
+  merge_script <- str_c(merge_script, " -cat ", i, ".mp4")
 }
 
-paste(merge_script, "merged.mp4") %>% write("merge_script.sh")
+str_c(merge_script, "merged.mp4", sep = " ") %>% write("merge_script.sh")
 
 
